ae0e892809c94adc427fbf9de89e83571d79b454
chohyejung <hj1021007@gmail.com>
2025-04-02 15:18:19 +0900

    css : hover, complete 변경 js : enter키로 타이핑 비활성화 추가

--- 01 todo-app/혜정/script.js
+++ 01 todo-app/혜정/script.js
@@ -1,14 +1,14 @@
-const list = document.getElementById('list');
-const createBtn = document.getElementById('create-btn');
+const list = document.getElementById("list");
+const createBtn = document.getElementById("create-btn");
 
 let todos = [];
 
-createBtn.addEventListener('click', createNewTodo);
+createBtn.addEventListener("click", createNewTodo);
 
 function createNewTodo() {
   const item = {
     id: new Date().getTime(),
-    text: '',
+    text: "",
     complete: false,
   };
 
@@ -22,69 +22,79 @@ function createNewTodo() {
   list.prepend(itemEl);
 
   //생성 시 타이핑 활성화
-  inputEl.removeAttribute('disabled');
+  inputEl.removeAttribute("disabled");
   inputEl.focus();
 
   saveToLocalStorage();
 }
 
 function createTodoElement(item) {
-  const itemEl = document.createElement('div');
-  itemEl.classList.add('item');
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
 
-  const checkboxEl = document.createElement('input');
-  checkboxEl.type = 'checkbox';
+  const checkboxEl = document.createElement("input");
+  checkboxEl.type = "checkbox";
   checkboxEl.checked = item.complete;
 
   //체크완료 시
   if (item.complete) {
-    itemEl.classList.add('complete');
+    itemEl.classList.add("complete");
   }
 
-  const inputEl = document.createElement('input');
-  inputEl.type = 'text';
+  const inputEl = document.createElement("input");
+  inputEl.type = "text";
   inputEl.value = item.text;
   //체크완료시 타이핑 막기
-  inputEl.setAttribute('disabled', '');
+  inputEl.setAttribute("disabled", "");
 
-  const actionsEl = document.createElement('div');
-  actionsEl.classList.add('actions');
+  const actionsEl = document.createElement("div");
+  actionsEl.classList.add("actions");
 
   //edit, remove 버튼
-  const editBtnEl = document.createElement('button');
-  editBtnEl.classList.add('material-icons');
-  editBtnEl.innerText = 'edit';
+  const editBtnEl = document.createElement("button");
+  editBtnEl.classList.add("material-icons");
+  editBtnEl.innerText = "edit";
 
-  const removeBtnEl = document.createElement('button');
-  removeBtnEl.classList.add('material-icons', 'remove-btn');
-  removeBtnEl.innerText = 'remove_circles';
+  const removeBtnEl = document.createElement("button");
+  removeBtnEl.classList.add("material-icons", "remove-btn");
+  removeBtnEl.innerText = "remove_circles";
 
-  inputEl.addEventListener('input', () => {
+  inputEl.addEventListener("input", () => {
     item.text = inputEl.value;
   });
 
-  checkboxEl.addEventListener('change', () => {
+  //enter키로 타이핑 비활성화
+  inputEl.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      inputEl.setAttribute("disabled", "");
+      saveToLocalStorage();
+    }
+  });
+
+  checkboxEl.addEventListener("change", () => {
     item.complete = checkboxEl.checked;
 
     if (item.complete) {
-      itemEl.classList.add('complete');
+      itemEl.classList.add("complete");
     } else {
-      itemEl.classList.remove('complete');
+      itemEl.classList.remove("complete");
     }
+
     saveToLocalStorage();
   });
 
-  inputEl.addEventListener('blur', () => {
-    inputEl.setAttribute('disabled', '');
+  inputEl.addEventListener("blur", () => {
+    inputEl.setAttribute("disabled", "");
     saveToLocalStorage();
   });
 
-  editBtnEl.addEventListener('click', () => {
-    inputEl.removeAttribute('disabled');
+  editBtnEl.addEventListener("click", () => {
+    inputEl.removeAttribute("disabled");
     inputEl.focus();
   });
 
-  removeBtnEl.addEventListener('click', () => {
+  removeBtnEl.addEventListener("click", () => {
     todos = todos.filter((t) => t.id !== item.id);
     //다른 id값만 새로운 todos 배열로 반환
     itemEl.remove();
@@ -108,12 +118,12 @@ function saveToLocalStorage() {
   const data = JSON.stringify(todos);
 
   //window. 생략
-  localStorage.setItem('my_todos', data);
+  localStorage.setItem("my_todos", data);
 }
 
 //로컬 스토리지 데이터 가져오기
 function loadFromLocalStorage() {
-  const data = localStorage.getItem('my_todos');
+  const data = localStorage.getItem("my_todos");
 
   //스트링을 배열로 변환
   if (data) {
