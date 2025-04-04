1a68b23ade85e1e958f1a96ca42a2f79f1ccac48
Jiwoo Han <hjw0235@naver.com>
2025-04-04 16:49:49 +0900

    외부 파일로 통합

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const list = document.getElementById("list");
 const createBtn = document.getElementById("create-btn");
+const toggleThemeBtn = document.querySelector('.header__theme-button');
 
 let todos = [];
 let deletedTodos = [];
@@ -364,5 +365,27 @@ function displayTodos() {
   }
   setupTrashModal();
 }
+
+window.onload = function() {
+  setInitialTheme(localStorage.getItem('theme'));
+}
+function setInitialTheme(themeKey) {
+  if(themeKey === 'dark') {
+      document.documentElement.classList.add('dark-mode');
+  } else {
+      document.documentElement.classList.remove('dark-mode');
+  }
+}
+      
+toggleThemeBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+
+  if(document.documentElement.classList.contains('dark-mode')) {
+      localStorage.setItem('theme', 'dark');
+  } else {
+      localStorage.setItem('theme', 'light');
+  }
+  updateThemeIcon();
+})
 
 displayTodos();
